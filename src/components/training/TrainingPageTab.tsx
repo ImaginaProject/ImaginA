eb5b42e58e52ecf17153048182c92657ed412b30
David Borja <davidborja456@gmail.com>
2022-11-10 08:34:00 -0500

    Add a component to handle models of an specific directory

---
+++ src/components/training/TrainingPageTab.tsx
@@ -0,0 +1,190 @@
+import { FunctionComponent, Key, useState, useEffect } from 'react'
+import dayjs from 'dayjs'
+import Loading from '../loading/Loading'
+import { Space, Table, Upload, Button, Input, Alert, Tooltip } from 'antd'
+import { DeleteOutlined, LoadingOutlined, UploadOutlined } from '@ant-design/icons'
+import type { ColumnsType } from 'antd/es/table'
+import type { UploadFile } from 'antd/es/upload/interface'
+import ToolDeleteModel from './ToolDeleteModel'
+
+
+type RegisteredModel = {
+  name: string,
+  description: string,
+  last_date: string,
+  path: string,
+  md5_sum: string,
+}
+
+interface DataType extends RegisteredModel {
+  key: Key,
+  model: any,
+}
+
+export interface TrainingPageTabProps {
+  directory: string,
+  title: string,
+  actionURL: string,
+}
+
+const TrainingPageTab: FunctionComponent<TrainingPageTabProps> = (props) => {
+  const [allModels, setAllModels] = useState<RegisteredModel[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [dataSource, setDataSource] = useState<DataType[]>([])
+  const [fileList, setFileList] = useState<UploadFile[]>([]);
+
+  const [modelName, setModelName] = useState('');
+  const [modelDescription, setModelDescription] = useState('');
+
+  const [isErrorAlertShown, setIsErrorAlertShown] = useState(false);
+  const [errorAlertMessage, setErrorAlertMessage] = useState('');
+
+  const columns: ColumnsType<DataType> = [
+    {
+      title: 'Nombre',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Descripción',
+      dataIndex: 'description',
+      key: 'description',
+    },
+    {
+      title: 'Última modificación',
+      dataIndex: 'last_date',
+      key: 'last_date',
+    },
+    {
+      title: 'Opciones',
+      dataIndex: 'model',
+      key: 'model',
+      render: (item: RegisteredModel) => {
+        return (
+          <ToolDeleteModel
+            deleteURL={`${props.actionURL}/${item.md5_sum}`}
+            onDelete={() => {
+              requestAllModels()
+            }}
+          />
+        )
+      },
+    }
+  ]
+
+  const requestAllModels = async () => {
+    setIsLoading(true)
+    // Request all the models
+    const responseAllModels = await fetch(props.actionURL)
+    const allModelsData = await responseAllModels.json()
+    setAllModels(allModelsData.models as RegisteredModel[])
+    // Finishs
+    setIsLoading(false)
+  }
+
+  const showErrorAlert = (message: string) => {
+    setErrorAlertMessage(message)
+    setIsErrorAlertShown(true)
+    setTimeout(() => setIsErrorAlertShown(false), 5000)
+  }
+
+  useEffect(() => {   
+    requestAllModels().then()
+  }, [])
+
+  useEffect(() => {
+    setDataSource(allModels.map((model) => ({
+      key: model.md5_sum,
+      ...model,
+      last_date: dayjs(model.last_date).format('DD/MM/YYYY'),
+      model,
+    })))
+  }, [allModels])
+
+  return (
+    <Space style={{ width: '100%' }} direction='vertical'>
+      <Input
+        placeholder='Model name'
+        value={modelName}
+        onChange={(e) => setModelName(e.target.value)}
+      />
+      <Input.TextArea
+        placeholder='Model description (optional)'
+        value={modelDescription}
+        onChange={(e) => setModelDescription(e.target.value)}
+      />
+      <Upload
+        disabled={!modelName.trim()}
+        name='model_file'
+        action={props.actionURL}
+        method='POST'
+        maxCount={1}
+        fileList={fileList}
+        showUploadList={{
+          showRemoveIcon: true,
+        }}
+        onChange={(info) => {
+          setFileList(info.fileList)
+          if (info.file.status === 'done') {
+            requestAllModels()
+          }
+        }}
+        customRequest={async (request) => {
+          const form = new FormData()
+          const payload = {
+            name: modelName,
+            description: modelDescription,
+          }
+          form.append('model_info', JSON.stringify(payload))
+          form.append(request.filename || 'model_file', request.file)
+
+          try {
+            request.onProgress && request.onProgress({percent: 10})
+            const response = await fetch(
+              request.action,
+              {
+                method: request.method,
+                body: form,
+                headers: { ...request.headers }
+              },
+            )
+
+            request.onProgress && request.onProgress({percent: 90})
+            const data = await response.json()
+            request.onProgress && request.onProgress({percent: 100})
+
+            if (response.status === 200) {
+              request.onSuccess && request.onSuccess(data)
+            } else {
+              showErrorAlert(`No puede subir archivo. Error: ${data['app_exception']}`)
+            }
+            setModelName('')
+            setModelDescription('')
+            setFileList([])
+          } catch(e) {
+            // TODO: check how to pass the error to `onError` because it spends
+            //       a type `UploadRequestError` or `ProgressEvent<EventTarget>`
+            request.onError && request.onError(e as any)
+          }
+        }}
+      >
+        <Button
+          disabled={!modelName.trim()}
+          icon={<UploadOutlined />}
+        >
+          Click para subir un modelo
+        </Button>
+      </Upload>
+      {isErrorAlertShown && (
+        <Alert message={errorAlertMessage} type='error' />
+      )}
+      {isLoading ? (
+        <Loading>Requesting all the models</Loading>
+      ) : (
+        <Table columns={columns} dataSource={dataSource}/>
+      )}
+    </Space>
+  )
+}
+
+export default TrainingPageTab
