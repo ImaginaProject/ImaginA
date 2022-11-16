0d31929b75ed001df0413db3a682981242c254fc
David Borja <davidborja456@gmail.com>
2022-11-16 16:07:41 -0500

    Add first page that shows and adds special days

--- src/components/special-days/SpecialDaysPage.tsx
+++ src/components/special-days/SpecialDaysPage.tsx
@@ -1,5 +1,17 @@
 import { FunctionComponent, useState, useEffect } from 'react'
-import { Space, DatePicker, Dropdown, Button, Menu, Typography } from 'antd'
+import {
+  Space,
+  DatePicker,
+  Checkbox,
+  Button,
+  List,
+  Typography,
+  Skeleton,
+  Form,
+  Divider,
+  Badge,
+  Alert,
+} from 'antd'
 import dayjs, { Dayjs } from 'dayjs'
 import 'dayjs/locale/es'
 import weekday from 'dayjs/plugin/weekday'
@@ -8,7 +20,9 @@ import customParseFormat from 'dayjs/plugin/customParseFormat'
 
 import Loading from '../loading/Loading'
 
-import { LoadingOutlined } from '@ant-design/icons'
+import { DeleteOutlined, LoadingOutlined } from '@ant-design/icons'
+import SpecialDays from '../../classes/SpecialDays'
+import type { SpecialDayResponse, SpecialDay } from '../../types/types'
 
 // Load the plugins
 dayjs.locale('es')
@@ -19,10 +33,129 @@ dayjs.extend(customParseFormat)
 
 export interface SpecialDaysPageProps {}
 
+const formItemLayout = {
+  labelCol: { span: 6 },
+  wrapperCol: { span: 14 },
+}
+
 const SpecialDaysPage: FunctionComponent<SpecialDaysPageProps> = (props) => {
+  const [sd, setSd] = useState(new SpecialDays())
+  const [isLoading, setIsLoading] = useState(false)
+  const [specialDays, setSpecialDays] = useState<SpecialDay[]>([])
+  const [errorAlertMessage, setErrorAlertMessage] = useState<string | null>(null);
+
+  const [form] = Form.useForm()
+
+  const requestAllSpecialDays = async () => {
+    setIsLoading(true)
+    try {
+      const allDays = await sd.getAll()
+      setSpecialDays(allDays.sort((a, b) => {
+        if (a.date > b.date) return 1
+        if (a.date < b.date) return -1
+        return 0
+      }))
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  const showErrorAlert = (message: string) => {
+    setErrorAlertMessage(message)
+    setTimeout(() => setErrorAlertMessage(null), 4000)
+  }
+
+  useEffect(() => {
+    requestAllSpecialDays().catch((reason) => console.error(reason))
+  }, [])
+
+  const onFormFinish = (value: any) => {
+    console.debug(value)
+    sd.add(value.date, value.isHoliday, value.isVacation)
+      .then((success) => {
+        console.log('saved?', success)
+        if (success) {
+          requestAllSpecialDays().catch((reason) => {
+            console.error(reason)
+            showErrorAlert(JSON.stringify(reason))
+          })
+        } else {
+          showErrorAlert('No puede agregar esa fecha. Verifique que no esté duplicada')
+        }
+      })
+      .finally(() => {
+        form.resetFields()
+      })
+  }
+
   return (
     <Space style={{padding: '2em', width: '100%'}} direction='vertical'>
-      hola
+      <Typography.Title>
+        Configuración de días especiales
+      </Typography.Title>
+  
+      {/* Form that asks for date */}
+      <Space direction='vertical' style={{ width: '100%'}}>
+        <Typography.Text strong>Agregar nuevo día</Typography.Text>
+        <Form {...formItemLayout} form={form} onFinish={onFormFinish}>
+          <Form.Item
+            name='date'
+            label='Fecha'
+            rules={[
+              {
+                required: true,
+                message: 'Please input the date',
+              },
+            ]}
+          >
+            <DatePicker/>
+          </Form.Item>
+          <Typography.Text strong>Configuración del día</Typography.Text>
+          <Form.Item label='Día es festivo' name='isHoliday' valuePropName='checked'>
+            <Checkbox value={true}>
+              Es festivo
+            </Checkbox>
+          </Form.Item>
+          <Form.Item label='Día es vacaciones' name='isVacation' valuePropName='checked'>
+            <Checkbox value={true}>
+              Es vacaciones
+            </Checkbox>
+          </Form.Item>
+          <Form.Item wrapperCol={{ span: 12, offset: 6 }}>
+            <Button type='primary' htmlType='submit'>
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Space>
+      {errorAlertMessage !== null && <Alert message={errorAlertMessage} type='error'/>}
+      <Divider/>
+      <List
+        dataSource={specialDays}
+        renderItem={(item: SpecialDay) => (
+          <List.Item
+            actions={[
+              <Button danger><DeleteOutlined/></Button>
+            ]}
+          >
+            <Skeleton title={false} loading={isLoading} active>
+              <Typography.Text strong>
+                Fecha: {dayjs(item.date).format('DD/MM/YYYY')}
+              </Typography.Text>
+              {item.isHoliday ? (
+                <Badge count='Festivo' style={{backgroundColor: 'greenyellow'}} />
+              ) : (
+                <Badge count='No festivo' style={{backgroundColor: 'orangered'}} />
+              )}
+              {item.isVacation ? (
+                <Badge count='Día vacaciones' style={{backgroundColor: 'red'}} />
+              ) : (
+                <Badge count='No es vacaciones' style={{backgroundColor: 'olivedrab'}} />
+              )}
+            </Skeleton>
+          </List.Item>
+        )}
+      />
     </Space>
   )
 }
