ae5b3f3fbfea54b3a35574cef9f4fa2eb07ea123
David Borja <davidborja456@gmail.com>
2022-11-22 18:24:55 -0500

    Add new locale items

--- src/locale/locale.ts
+++ src/locale/locale.ts
@@ -2,10 +2,12 @@ import type { LocaleSetType } from './types'
 
 const localeSet: LocaleSetType = {
   en: {
-    'menu.dataset.manager.description': 'Add, edit and delete dataset items',
+    'menu.dataset.manager.description': 'add, edit and delete dataset items',
+    'menu.dataset.manager.title': 'dataset manager',
   },
   es: {
-    'menu.dataset.manager.description': 'Agrega, edita y elimina registros del dataset',
+    'menu.dataset.manager.description': 'agrega, edita y elimina registros del dataset',
+    'menu.dataset.manager.title': 'administrador de dataset',
   },
 }
 
