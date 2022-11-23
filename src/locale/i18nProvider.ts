5af82aa061e768e48c878170f7a3d753ae0016f7
David Borja <davidborja456@gmail.com>
2022-11-23 11:09:54 -0500

    Improve the i18n provider

--- src/locale/i18nProvider.ts
+++ src/locale/i18nProvider.ts
@@ -1,19 +1,39 @@
 import type { AvailableLocale, LocaleType } from './types'
 import localeSet from './locale'
 
+type OptionsType = {
+  exclamation?: boolean,
+  question?: boolean,
+  start?: boolean,
+  end?: boolean,
+  default?: string,
+}
+
 let currentLocale: AvailableLocale = 'es'
 let localeData: LocaleType = localeSet.es
 
 // eslint-disable-next-line import/prefer-default-export
 export const i18nProvider = {
   // required
-  translate: (key: keyof LocaleType, options: any) => {
-    let words: string = localeData[key] || options.default || localeSet.es[key] || '???'
+  translate: (key: keyof LocaleType, options?: OptionsType) => {
+    let words: string = localeData[key] || options?.default || localeSet.es[key] || '???'
     if (options?.start) {
       words = words.charAt(0).toUpperCase() + words.substring(1)
     }
     if (options?.end) {
       words += '.'
+    }
+    if (options?.exclamation) {
+      if (currentLocale === 'es') {
+        words = `¡${words}`
+      }
+      words = `${words}!`
+    }
+    if (options?.question) {
+      if (currentLocale === 'es') {
+        words = `¿${words}`
+      }
+      words = `${words}?`
     }
     return words
   },
