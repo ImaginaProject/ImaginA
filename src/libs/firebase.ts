1350772ba6b3479b3592ce10a203e25ae1de926b
David Borja <davidborja456@gmail.com>
2022-11-25 19:16:27 -0500

    Start using firebase with custom config

---
+++ src/libs/firebase.ts
@@ -0,0 +1,35 @@
+import { initializeApp } from 'firebase/app'
+// import { getAnalytics } from 'firebase/analytics'
+
+import {
+  getDatabase,
+  ref,
+  onValue,
+  get,
+  child,
+} from 'firebase/database'
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_apiKey,
+  authDomain: import.meta.env.VITE_APP_authDomain,
+  databaseURL: import.meta.env.VITE_APP_databaseURL,
+  projectId: import.meta.env.VITE_APP_projectId,
+  storageBucket: import.meta.env.VITE_APP_storageBucket,
+  messagingSenderId: import.meta.env.VITE_APP_messagingSenderId,
+  appId: import.meta.env.VITE_APP_appId,
+  measurementId: import.meta.env.VITE_APP_measurementId,
+}
+
+const app = initializeApp(firebaseConfig)
+
+const realtimeDB = getDatabase(app)
+// const analytics = getAnalytics(app)
+
+export default {
+  realtimeDB,
+  ref,
+  onValue,
+  get,
+  child,
+  // analytics,
+}
