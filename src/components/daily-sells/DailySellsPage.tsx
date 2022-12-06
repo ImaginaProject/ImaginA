d2421f5903aa55e3198698990f47330c6bc20054
David Borja <davidborja456@gmail.com>
2022-12-06 12:01:23 -0500

    Update the DailySellsPage

--- src/components/daily-sells/DailySellsPage.tsx
+++ src/components/daily-sells/DailySellsPage.tsx
@@ -4,8 +4,7 @@ import {
   useEffect,
   useCallback,
 } from 'react'
-import dayjs, { Dayjs } from 'dayjs'
-
+// import dayjs, { Dayjs } from 'dayjs'
 import {
   Space,
   DatePicker,
@@ -16,14 +15,48 @@ import {
   Typography,
   InputNumber,
   Button,
+  Image,
 } from 'antd'
+
+import DailyCapacity from '../../classes/DailyCapacity'
+import { ExistentModel } from '../../types/types'
+
+const ENDPOINT = import.meta.env.VITE_APP_ENDPOINT
 
 export interface DailySellsProps {}
 
 const DailySells: FunctionComponent<DailySellsProps> = () => {
+  const [dc] = useState(new DailyCapacity());
+  const [possibleTrainedModelList, setPossibleTrainedModelList] = useState<ExistentModel[]>([]);
+  const [imageB64, setImageB64] = useState<string | null>(null);
   const [form] = Form.useForm()
 
-  const onFormFinish = useCallback((values: any) => {}, [])
+  const onFormFinish = useCallback((values: any) => {
+    console.log('form:', values)
+    const payload = {
+      date: values.date.format('DD/MM/YYYY'),
+      min_price: values.minPrice,
+      max_price: values.maxPrice,
+      model_id: values.modelId,
+    }
+
+    fetch(`${ENDPOINT}/daily-capacity/optimize-prices`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify(payload),
+    }).then((response) => response.json())
+      .then((data) => setImageB64(data.image))
+      .catch((err) => console.error(err))
+  }, [])
+
+  useEffect(() => {
+    dc.requestAllModels().then((models) => {
+      console.debug('got all models', models)
+      setPossibleTrainedModelList(dc.possibleTrainedModels)
+    })
+  }, [])
 
   return (
     <Space style={{ padding: '2em', width: '100%' }} direction="vertical">
@@ -33,22 +66,52 @@ const DailySells: FunctionComponent<DailySellsProps> = () => {
           Inserte datos de predicción
         </Typography.Text>
         <Form form={form} onFinish={onFormFinish}>
-          <Form.Item label="Modelo" name="modelId">
-            <Select style={{ width: '100%' }} />
+          <Form.Item
+            label="Modelo"
+            name="modelId"
+            rules={[
+              { required: true, message: 'Seleccione un modelo para usar' },
+            ]}
+          >
+            <Select
+              style={{ width: '100%' }}
+              options={possibleTrainedModelList.map((model) => ({
+                label: model.name,
+                value: model.id,
+              }))}
+            />
           </Form.Item>
 
-          <Form.Item label="Fecha" name="date">
+          <Form.Item
+            label="Fecha"
+            name="date"
+            rules={[
+              { required: true, message: 'La fecha es requerida' },
+            ]}
+          >
             <DatePicker />
           </Form.Item>
 
           <Row gutter={16}>
             <Col md={12} sm={24}>
-              <Form.Item label="Precio mínimo" name="minPrice">
+              <Form.Item
+                label="Precio mínimo"
+                name="minPrice"
+                rules={[
+                  { required: true, message: 'Fije un precio mínimo' },
+                ]}
+              >
                 <InputNumber min={0} />
               </Form.Item>
             </Col>
             <Col md={12} sm={24}>
-              <Form.Item label="Precio máximo" name="maxPrice">
+              <Form.Item
+                label="Precio máximo"
+                name="maxPrice"
+                rules={[
+                  { required: true, message: 'Fije un precio máximo' },
+                ]}
+              >
                 <InputNumber min={0} />
               </Form.Item>
             </Col>
@@ -62,6 +125,7 @@ const DailySells: FunctionComponent<DailySellsProps> = () => {
         </Form>
       </Space>
 
+      {imageB64 && <Image width="100%" src={imageB64} />}
     </Space>
   )
 }
