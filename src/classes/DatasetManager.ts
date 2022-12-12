3797e2f685e5435dc01c66ed8622e2b24f5a42a0
David Borja <davidborja456@gmail.com>
2022-12-12 16:49:34 -0500

    Revert "Change all the dataset manager system"
    This reverts commit e777f1dedd4deb72591b28fae4b86d160ea6d9a3.

--- src/classes/DatasetManager.ts
+++ src/classes/DatasetManager.ts
@@ -1,23 +1,10 @@
-import { DatasetFileDescription, DatasetFile } from '../types/types'
-
-type DatasetFileResponse = {
-  id: string
-  name: string
-  description?: string
-  type: 'csv' | 'xlsx'
-  columns: string[]
-  column_length: number
-  row_length: number
-  file_size: number
-  resource: string
-  md5_sum: string
-  date: Date
-}
+import dayjs from 'dayjs'
+import { DailyCapacityDB } from '../types/types'
 
 export default class DatasetManager {
   private endpoint: string
 
-  public datasetList: DatasetFile[]
+  public datasetList: DailyCapacityDB[]
 
   constructor() {
     this.endpoint = import.meta.env.VITE_APP_ENDPOINT
@@ -25,49 +12,55 @@ export default class DatasetManager {
   }
 
   async requestAll() {
-    const response = fetch(`${this.endpoint}/datasets/files`)
+    const response = fetch(`${this.endpoint}/datasets`)
     const data = await (await response).json()
-    this.datasetList = data.list.map((item: DatasetFileResponse) => {
-      const processedData: DatasetFile = {
-        name: item.name,
-        description: item.description,
+    this.datasetList = data.entries.map((item: any) => {
+      const processedData: DailyCapacityDB = {
         date: item.date,
-        columnLength: item.column_length,
-        columns: item.columns,
-        fileSize: item.file_size,
+        footfall: item.footfall,
         id: item.id,
-        md5Sum: item.md5_sum,
-        resource: item.resource,
-        rowLength: item.row_length,
-        type: item.type,
+        isHoliday: item.is_holiday,
+        isVacation: item.is_vacation,
       }
       return processedData
     })
     return this.datasetList
   }
 
-  // eslint-disable-next-line class-methods-use-this
-  async add(data: DatasetFileDescription) {
-    const payload = {
-      name: data.name,
-      description: data.description,
-      md5_sum_with_ext: data.resource,
-      type: data.type,
+  async deleteById(id: string) {
+    const response = await fetch(`${this.endpoint}/datasets/${id}`, { method: 'DELETE' })
+    const data = await response.json()
+    if (response.status === 200) {
+      console.debug('deleting process responses:', data)
+      return data.success as boolean
     }
 
-    const response = await fetch(`${this.endpoint}/datasets/file`, {
+    console.warn('deleting process got status code:', response.status)
+    return false
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  async add(data: DailyCapacityDB) {
+    const payload = {
+      date: dayjs(data.date).format('DD/MM/YYYY'),
+      is_holiday: data.isHoliday,
+      is_vacation: data.isVacation,
+      footfall: data.footfall,
+    }
+    const response = await fetch(`${this.endpoint}/datasets`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json;charset=utf-8',
       },
       body: JSON.stringify(payload),
     })
-
-    const json: DatasetFileResponse = await response.json()
+    const json = await response.json()
     if (response.status === 200) {
       console.debug('addding entry got json:', json)
-    } else {
-      console.warn('got status code:', response.status, json)
+      return json.success as boolean
     }
+
+    console.warn('got status code:', response.status, data)
+    return false
   }
 }
--- src/components/dataset/DatasetManagerPage.tsx
+++ src/components/dataset/DatasetManagerPage.tsx
@@ -2,67 +2,212 @@ import {
   FunctionComponent,
   useEffect,
   useState,
+  ReactNode,
 } from 'react'
+import dayjs from 'dayjs'
 import {
   Space,
   Typography,
+  Table,
   Button,
   Divider,
+  Modal,
   Form,
-  Input,
-  Radio,
-  List,
-  Card,
-  Tag,
-  Tooltip,
+  DatePicker,
+  Checkbox,
+  InputNumber,
 } from 'antd'
-import {
-  DeleteOutlined,
-} from '@ant-design/icons'
 import { useTranslate } from 'react-admin'
-import Uploader from '../utils/Uploader'
+import type { ColumnsType } from 'antd/es/table'
+import { DeleteOutlined, LoadingOutlined } from '@ant-design/icons'
 import DatasetManager from '../../classes/DatasetManager'
-import { DatasetFile } from '../../types/types'
+import type { DailyCapacityDB } from '../../types/types'
 
 export interface DatasetManagerPageProps {}
 
-const MimetypeToType = {
-  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': 'xlsx',
-  'text/csv': 'csv',
+interface DeleteButtonProps {
+  children: ReactNode,
+  onDelete: () => Promise<void>,
 }
+const DeleteButton: FunctionComponent<DeleteButtonProps> = (props) => {
+  const { children, onDelete } = props
+  const [isLoading, setIsLoading] = useState(false)
 
-const TypeToColor = {
-  xlsx: 'magenta',
-  csv: 'blue',
+  const onDeleteClick = async () => {
+    setIsLoading(true)
+    try {
+      const success = await onDelete()
+      console.info('deleting process successful:', success)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return (
+    <Button
+      disabled={isLoading}
+      onClick={onDeleteClick}
+      danger
+    >
+      {isLoading ? (
+        <LoadingOutlined />
+      ) : (
+        children
+      )}
+    </Button>
+  )
 }
 
 const DatasetManagerPage: FunctionComponent<DatasetManagerPageProps> = () => {
   const [dm] = useState(new DatasetManager())
+  const [isLoading, setIsLoading] = useState(false)
+  const [dataSource, setDataSource] = useState<(DailyCapacityDB & { key: any })[]>([])
 
-  const [datasetFiles, setDatasetFiles] = useState<DatasetFile[]>([]);
+  const [isAddingFormShown, setIsAddingFormShown] = useState(false)
+  const [isAddingFormSubmiting, setIsAddingFormSubmiting] = useState(false)
 
-  const [form] = Form.useForm()
-
+  const [addingForm] = Form.useForm()
   const translate = useTranslate()
 
-  const onFormFinish = (values: any) => {
-    console.log('values:', values)
-
-    if ((values.fileList as any[]).length === 0) {
-      console.error('no fileList found:', values)
-      return
-    }
-
-    dm.add({
-      name: values.name,
-      resource: values.fileList[0].response.file,
-      type: values.type,
-      description: values.description,
-    }).then(() => dm.requestAll().then(setDatasetFiles))
+  const openAddingForm = () => {
+    setIsAddingFormShown(true)
   }
 
+  const closeAddingForm = () => {
+    addingForm.resetFields()
+    setIsAddingFormShown(false)
+  }
+
+  const requestAllEntries = async () => {
+    setIsLoading(true)
+
+    try {
+      await dm.requestAll()
+      setDataSource(dm.datasetList.map((item, key) => ({ key, ...item })))
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  const onFinishAddingForm = (value: any) => {
+    const {
+      date,
+      isHoliday,
+      isVacation,
+      footfall,
+    } = value
+    console.debug('save', value)
+
+    setIsAddingFormSubmiting(true)
+
+    dm.add({
+      id: '', // Empty because this field is set by the Back-End
+      date,
+      isHoliday: !!isHoliday, // Avoid send undefined, send false instead
+      isVacation: !!isVacation, // Avoid send undefined, send false instead
+      footfall,
+    }).then((success) => {
+      if (success) {
+        requestAllEntries()
+      }
+    }).finally(() => {
+      setIsAddingFormSubmiting(false)
+      closeAddingForm()
+    })
+  }
+
+  const columns: ColumnsType<DailyCapacityDB> = [
+    {
+      title: translate('imagina.word.date'),
+      key: 'date',
+      dataIndex: 'date',
+      render: (item: Date) => dayjs(item).format('DD/MM/YYYY'),
+      sorter: (a, b) => {
+        if (a.date === b.date) return 0
+        if (a.date > b.date) return 1
+        return -1
+      },
+    },
+    {
+      title: translate('imagina.word.holiday'),
+      key: 'holiday',
+      dataIndex: 'isHoliday',
+      render: (item: boolean) => (item ? 'Sí' : 'No'),
+      filters: [
+        {
+          text: translate('imagina.holiday.is'),
+          value: true,
+        },
+        {
+          text: translate('imagina.holiday.is_not'),
+          value: false,
+        },
+      ],
+      onFilter: (value, record) => record.isHoliday === value,
+    },
+    {
+      title: translate('imagina.word.vacation'),
+      key: 'vacation',
+      dataIndex: 'isVacation',
+      render: (item: boolean) => (item ? 'Sí' : 'No'),
+      filters: [
+        {
+          text: translate('imagina.vacation.is'),
+          value: true,
+        },
+        {
+          text: translate('imagina.vacation.is_not'),
+          value: false,
+        },
+      ],
+      onFilter: (value, record) => record.isVacation === value,
+    },
+    {
+      title: translate('imagina.word.footfall'),
+      key: 'capacity',
+      dataIndex: 'footfall',
+      filters: [
+        {
+          text: '>= 10',
+          value: 10,
+        },
+        {
+          text: '>= 100',
+          value: 100,
+        },
+        {
+          text: '>= 1000',
+          value: 1000,
+        },
+        {
+          text: '>= 10000',
+          value: 10000,
+        },
+      ],
+      onFilter: (value, record) => record.footfall >= value,
+    },
+    {
+      title: translate('imagina.word.options'),
+      key: 'options',
+      render: (item: DailyCapacityDB) => (
+        <Space>
+          <DeleteButton
+            onDelete={async () => {
+              const success = await dm.deleteById(item.id)
+              if (success) {
+                await requestAllEntries()
+              }
+            }}
+          >
+            <DeleteOutlined />
+          </DeleteButton>
+        </Space>
+      ),
+    },
+  ]
+
   useEffect(() => {
-    dm.requestAll().then(setDatasetFiles)
+    requestAllEntries()
   }, [])
 
   return (
@@ -74,109 +219,96 @@ const DatasetManagerPage: FunctionComponent<DatasetManagerPageProps> = () => {
         {translate('imagina.dataset.manager.description')}
       </Typography.Text>
 
-      <Form form={form} onFinish={onFormFinish}>
-        <Form.Item
-          name="name"
-          label="Nombre del dataset"
-          rules={[{ required: true, message: 'El nombre del dataset es requerido' }]}
-        >
-          <Input placeholder="Nombre del datasset" />
-        </Form.Item>
-
-        <Form.Item name="description" label="Descripción del dataset">
-          <Input.TextArea placeholder="descripción del dataset (optional)" />
-        </Form.Item>
-
-        <Form.Item
-          label="Tipo de archivo"
-          name="type"
-          rules={[{ required: true, message: 'El tipo de archivo es requerido' }]}
-        >
-          <Radio.Group>
-            <Radio value="csv">CSV file</Radio>
-            <Radio value="xlsx">XLSX file</Radio>
-          </Radio.Group>
-        </Form.Item>
-
-        <Form.Item
-          name="fileList"
-          label="Archivo del modelo"
-          rules={[
-            {
-              required: true,
-              message: 'Archivo requerido',
-            },
-          ]}
-        >
-          <Uploader
-            name="file"
-            label="Click para subir un modelo"
-            enableType={Object.keys(MimetypeToType)}
-          />
-        </Form.Item>
-
-        <Form.Item>
-          <Button
-            htmlType="submit"
-          >
-            Agregar modelo
-          </Button>
-        </Form.Item>
-      </Form>
+      <Space direction="horizontal">
+        <Button onClick={openAddingForm} type="primary">
+          {translate('imagina.dataset.manager.add_element')}
+        </Button>
+        {/* eslint-disable-next-line no-alert */}
+        <Button disabled onClick={() => alert('Not implement yet')}>
+          {translate('imagina.dataset.manager.add_from_file')}
+        </Button>
+      </Space>
 
       <Divider />
+      <Table columns={columns} dataSource={dataSource} loading={isLoading} />
 
-      <List
-        dataSource={datasetFiles}
-        renderItem={(item) => (
-          <List.Item key={item.id}>
-            <Card
-              style={{ width: '100%' }}
-              title={(
-                <>
-                  <Tag color={TypeToColor[item.type] || 'red'}>
-                    {item.type}
-                  </Tag>
-                  {`Dataset "${item.name}"`}
-                </>
-              )}
-              actions={[
-                <Tooltip title="Eliminar">
-                  <Button
-                    danger
-                    icon={<DeleteOutlined />}
-                    onClick={() => { console.log('TODO: implement it') }}
-                  />
-                </Tooltip>,
-              ]}
-            >
-              <p>
-                {'Descripción: '}
-                {item.description || '<sin descripción>'}
-              </p>
-
-              <p>
-                {`Dataset de ${item.columnLength} columnas por ${item.rowLength} filas`}
-              </p>
-
-              <p>
-                {'Tamaño: '}
-                {item.fileSize}
-                {' bytes'}
-              </p>
-
-              <p>
-                {'Columnas: '}
-                {item.columns.map((column) => <Tag key={column}>{column}</Tag>)}
-              </p>
-
-              <small>
-                {`Creado el: ${item.date}`}
-              </small>
-            </Card>
-          </List.Item>
-        )}
-      />
+      <Modal
+        footer={false}
+        open={isAddingFormShown}
+        onCancel={closeAddingForm}
+      >
+        <Typography.Title>
+          {translate('imagina.dataset.modal.add_data')}
+        </Typography.Title>
+        <Form
+          form={addingForm}
+          onFinish={onFinishAddingForm}
+          initialValues={{
+            date: dayjs(new Date()),
+          }}
+        >
+          <Form.Item
+            name="date"
+            label={translate('imagina.word.date')}
+            rules={[
+              {
+                required: true,
+                message: translate('imagina.dataset.manager.write_date'),
+              },
+            ]}
+          >
+            <DatePicker />
+          </Form.Item>
+          <Form.Item
+            label={translate('imagina.holiday.day')}
+            name="isHoliday"
+            valuePropName="checked"
+          >
+            <Checkbox value>{translate('imagina.holiday.is')}</Checkbox>
+          </Form.Item>
+          <Form.Item
+            label={translate('imagina.vacation.day')}
+            name="isVacation"
+            valuePropName="checked"
+          >
+            <Checkbox value>{translate('imagina.vacation.is')}</Checkbox>
+          </Form.Item>
+          <Form.Item
+            label={translate('imagina.word.footfall')}
+            name="footfall"
+            rules={[
+              {
+                required: true,
+                message: translate('imagina.dataset.manager.write_footfall'),
+              },
+              {
+                type: 'integer',
+                message: translate('imagina.form.error.required_integer'),
+              },
+              {
+                validator: (_, value) => {
+                  if (value < 0) {
+                    return Promise.reject(
+                      new Error(translate('imagina.form.error.require_positive_integer')),
+                    )
+                  }
+                  return Promise.resolve()
+                },
+              },
+            ]}
+          >
+            <InputNumber />
+          </Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            disabled={isAddingFormSubmiting}
+            icon={(isAddingFormSubmiting && <LoadingOutlined />) || undefined}
+          >
+            {translate('imagina.word.submit')}
+          </Button>
+        </Form>
+      </Modal>
     </Space>
   )
 }
