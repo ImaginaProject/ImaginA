9a2f931b4f89352f714fe2fab58c876571ecfbfb
David Borja <davidborja456@gmail.com>
2022-12-22 09:39:51 -0500

    Update version number to 1.11.0

--- src/components/main/logs.ts
+++ src/components/main/logs.ts
@@ -1,79 +1,83 @@
 const tagMessages = [
   {
     tag: '1.0.0',
-    message: ' This version contains a basic page with the daily capacity feature and special days settings feature',
+    message: 'This version contains a basic page with the daily capacity feature and special days settings feature',
   },
   {
     tag: '1.0.1',
-    message: ' Show the current version in the main layout',
+    message: 'Show the current version in the main layout',
   },
   {
     tag: '1.0.2',
-    message: ' Add ESLint rules',
+    message: 'Add ESLint rules',
   },
   {
     tag: '1.2.0',
-    message: ' Version with a Calendar component and antimissing endpoint system',
+    message: 'Version with a Calendar component and antimissing endpoint system',
   },
   {
     tag: '1.2.1',
-    message: ' Rename some component name with important changes',
+    message: 'Rename some component name with important changes',
   },
   {
     tag: '1.3.0',
-    message: ' This version adds i18n supports',
+    message: 'This version adds i18n supports',
   },
   {
     tag: '1.4.0',
-    message: ' This version implements the dataset manager page',
+    message: 'This version implements the dataset manager page',
   },
   {
     tag: '1.4.1',
-    message: ' Use the locale in the dataset manager page',
+    message: 'Use the locale in the dataset manager page',
   },
   {
     tag: '1.5.0',
-    message: ' This version has a page to ask for retrainings and upload files arbitrarily',
+    message: 'This version has a page to ask for retrainings and upload files arbitrarily',
   },
   {
     tag: '1.5.1',
-    message: ' Update: Improve the page RetrainingListPage and send training parameters',
+    message: 'Update: Improve the page RetrainingListPage and send training parameters',
   },
   {
     tag: '1.5.2',
-    message: ' This version improve the daily capacity page',
+    message: 'This version improve the daily capacity page',
   },
   {
     tag: '1.5.3',
-    message: ' This version improves the locale system with official and custom translations',
+    message: 'This version improves the locale system with official and custom translations',
   },
   {
     tag: '1.6.0',
-    message: ' This version implements the page that shows the training report plots',
+    message: 'This version implements the page that shows the training report plots',
   },
   {
     tag: '1.7.0',
-    message: ' New version with new icons, some endpoints changed because the Back-End is now 2.x.x, and adds new page to the price optimizer',
+    message: 'New version with new icons, some endpoints changed because the Back-End is now 2.x.x, and adds new page to the price optimizer',
   },
   {
     tag: '1.8.0',
-    message: ' This version adds method to delete retraining logs, changed all the dataset system, adds new uploader component, new page of daily sells and new favicon adds',
+    message: 'This version adds method to delete retraining logs, changed all the dataset system, adds new uploader component, new page of daily sells and new favicon adds',
   },
   {
     tag: '1.8.1',
-    message: ' Fix getting data from daily sells response, improve some Tabs component using and update i18n',
+    message: 'Fix getting data from daily sells response, improve some Tabs component using and update i18n',
   },
   {
     tag: '1.8.2',
-    message: ' This little version fixes some i18n issues, a missing price value in daily capacity dataset page and adds an Spin component for loading',
+    message: 'This little version fixes some i18n issues, a missing price value in daily capacity dataset page and adds an Spin component for loading',
   },
   {
     tag: '1.9.0',
-    message: ' This version creates a page to create models and redirect to the retraining page with the created model ID or the daily sells page',
+    message: 'This version creates a page to create models and redirect to the retraining page with the created model ID or the daily sells page',
   },
   {
     tag: '1.10.0',
-    message: ' This version first updates the Uploader component and after implements a button to upload dataset from files',
+    message: 'This version first updates the Uploader component and after implements a button to upload dataset from files',
+  },
+  {
+    tag: '1.11.1',
+    message: 'This finishs the retraing page details and adds a ChangeLog component with logs by tags',
   },
 ].reverse()
 
